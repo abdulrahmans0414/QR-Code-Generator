a3bd7effd441b753efaf05c23f166ad3b945e0d1
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-10 03:20:03 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+// Use the inquirer npm package to get user input.
+// Use the qr-image npm package to turn the user entered URL into a QR code image.
+// Create a txt file to save the user input using the native fs node module.
