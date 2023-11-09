39122681c62aa563b1c66a66915cb1ed809f83f3
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-10 03:35:29 +0530

    add example code of inquirer from inquirer npm

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 // Use the inquirer npm package to get user input.
 // Use the qr-image npm package to turn the user entered URL into a QR code image.
 // Create a txt file to save the user input using the native fs node module.
+
+import inquirer from "inquirer";
+
+
+
+inquirer
+    .prompt([
+        /* Pass your questions in here */
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
